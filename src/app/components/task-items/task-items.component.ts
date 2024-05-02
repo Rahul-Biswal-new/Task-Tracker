620d17ea433b599e354513439b9946a6b047d172
Rahul-Biswal-new <rahulbiswalnew@gmail.com>
2024-05-02 20:55:11 +0530

    all

--- src/app/components/task-items/task-items.component.ts
+++ src/app/components/task-items/task-items.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-items',
   templateUrl: './task-items.component.html',
-  styleUrls: ['./task-items.component.css']
+    styleUrls: ['./task-items.component.css']
 })
 export class TaskItemsComponent {
-@Input() task!: Task
+@Input() task!: Task;
+faTimes = faTimes;
 }
